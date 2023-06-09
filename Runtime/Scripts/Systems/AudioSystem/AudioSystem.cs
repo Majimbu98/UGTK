16baa776d1562d9ed1e45eda949b0cba205a4d2d
marcello de bonis <marcello98.videogames@gmail.com>
2023-06-09 11:09:23 +0200

    Error System modified

--- Runtime/Scripts/Systems/AudioSystem/AudioSystem.cs
+++ Runtime/Scripts/Systems/AudioSystem/AudioSystem.cs
@@ -101,8 +101,8 @@ namespace UnityGamesToolkit.Runtime
             }
             else
             {
-                Debug.LogError("Error: S_Audio cannot be null!!!");
-                UnityEditor.EditorApplication.isPlaying = false;
+                Debug.LogError("Error: "+ typeof(S_Audio).ToString()+" cannot be null");
+                
             }
         }
 
@@ -117,8 +117,8 @@ namespace UnityGamesToolkit.Runtime
             }
             else
             {
-                Debug.LogError("Error: S_Audio cannot be null!!!");
-                UnityEditor.EditorApplication.isPlaying = false;
+                Debug.LogError("Error: "+ typeof(S_Audio).ToString()+" cannot be null");
+                
             }
         }
 
