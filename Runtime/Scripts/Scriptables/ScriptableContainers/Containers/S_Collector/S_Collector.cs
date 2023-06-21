b9249e9cb6064a218366419cace21c0fcbf6fbe6
marcello de bonis <marcello98.videogames@gmail.com>
2023-06-21 20:45:24 +0200

    Ajustment to code

--- Runtime/Scripts/Scriptables/ScriptableContainers/Containers/S_Collector/S_Collector.cs
+++ Runtime/Scripts/Scriptables/ScriptableContainers/Containers/S_Collector/S_Collector.cs
@@ -9,7 +9,7 @@ namespace UnityGamesToolkit.Runtime
     /// Class representing a collector of ScriptableObject objects.
     /// </summary>
     /// <typeparam name="T">The type of ScriptableObject that can be collected.</typeparam>
-    public class S_Collector<T> : ScriptableObject where T : ScriptableObject
+    public class S_Collector<T> : ScriptableObject
     {
         // Variables and properties
 
