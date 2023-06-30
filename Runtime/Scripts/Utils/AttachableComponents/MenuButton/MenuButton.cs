1ca4017a66c10bdbe62d06979fdb8d0621201b65
marcello de bonis <marcello98.videogames@gmail.com>
2023-06-30 08:30:18 +0200

    Menu button modified

--- Runtime/Scripts/Utils/AttachableComponents/MenuButton/MenuButton.cs
+++ Runtime/Scripts/Utils/AttachableComponents/MenuButton/MenuButton.cs
@@ -45,6 +45,15 @@ public void ChangeObjectInScene()
     }
 }
 
+public void QuitGame()
+{
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+}
+
 #endregion
 
 }
